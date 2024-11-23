29215574d2fc3095bdf2a3997c640526659ed781
Tpascal1 <tpascal1@ufl.edu>
2024-11-23 18:08:51 -0500

    Add files via upload

--- Lab11.py
+++ Lab11.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import matplotlib.pyplot as plt
 
 def read_students():
@@ -93,19 +94,28 @@ def main():
     assignments = read_assignments()
     submissions = read_submissions()
 
+    if len(sys.argv) <2:
+        print("Usage : script.py <choice> [<student_name>|<assigsnment_name>]")
+        return
+
+
+
     while True:
         print("\n1. Student grade")
         print("2. Assignment statistics")
         print("3. Assignment graph")
         choice = input("Enter your choice: ")
 
-        if choice == "1":
+        if choice == "1" and len(sys.argv) > 2 :
+            student_name = sys.argv[2]
             student_name = input("What is the student's name: ")
             calculate_grade(student_name, students, assignments, submissions)
-        elif choice == "2":
+        elif choice == "2" and len(sys.argv) > 2 :
+            assignmnet_name = sys.argv[2]
             assignment_name = input("What is the assignment name: ")
             assignment_stats(assignment_name, assignments, submissions)
-        elif choice == "3":
+        elif choice == "3" and len(sys.argv) > 2 :
+            assignment_name = sys.argv[2]
             assignment_name = input("What is the assignment name: ")
             assignment_graph(assignment_name, assignments, submissions)
         else:
